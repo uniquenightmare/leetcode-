38727a4676dd0d9b2d7013f4ce9067ff7486cedf
uniquenightmare <L1871937346@163.com>
2020-08-26 22:27:01 +0800

    2020-8-26

---
+++ 剑指offer68-1.cpp
@@ -0,0 +1,9 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if (root == NULL)return NULL;
+	if (root == p || root == q)return root;
+	TreeNode* left = lowestCommonAncestor(root->left, p, q);
+	TreeNode* right = lowestCommonAncestor(root->right, p, q);
+	if (left && right)return root;
+	return left ? left : right;
+}
